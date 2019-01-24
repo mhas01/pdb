d9db4daff0419c12b7f619cbbf4e5515db9cb104
AGUILAR SABORIO MANUEL HUMBERTO <AGUILARSM@bccr.fi.cr>
2019-01-24 10:26:31 -0600

    HTTP puppetdb listener

--- puppet/puppetdb/init.ps1
+++ puppet/puppetdb/init.ps1
@@ -1,2 +1,2 @@
 docker build . -t puppetdb
-docker run -d --hostname puppetdb --network puppetpoc --name puppetdb -p 23:22 -p 8081:8081 -it puppetdb bash
+docker run -d --hostname puppetdb --network puppetpoc --name puppetdb -p 23:22 -p 8081:8081 -p 8080:8080 -it puppetdb bash
